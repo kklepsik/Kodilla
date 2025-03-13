7957dd7d851b4af4686bc355af4dccdd51cd535c
Kleps Krzysztof <kkleps@intercity.pl>
2025-03-13 17:25:23 +0100

    Funkcja palindrom

--- palindromy.py
+++ palindromy.py
@@ -1,2 +1,12 @@
 print("Palindromy")
 
+"""porownanie slowa z odwroconym stringiem tego slowa
+nastepnie wydrukuj czy jest lub nie jest palindromem"""
+
+def palindrom(slowo):
+    if slowo == slowo[::-1]:
+        return print(True)
+    else:
+        return print(False)
+    
+palindrom("potop")
