4a38613a7bbdcc146179a9ad65b13a8decc69d77
Kleps Krzysztof <kkleps@intercity.pl>
2025-03-14 13:39:17 +0100

    Zadanie liczby

--- orly_3.py
+++ orly_3.py
@@ -1,2 +1,23 @@
 print('Zadania dla orłów - moduł 3')
 
+print('Zadanie 1')
+
+numbers = [5,32,56,2,2,16,7,10,23,100]
+mean_number = 0
+
+for index in range(len(numbers)):
+  numbers[index] = round(numbers[index], -1)
+
+numbers.sort()
+minimum = min(numbers)
+maximum = max(numbers)
+
+numbers = [number for number in numbers if number != minimum and number != maximum]
+
+print(numbers)
+
+
+mean_number = sum(numbers)/len(numbers)
+
+print(round(mean_number))
+
