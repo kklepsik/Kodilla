6eb230bb2e33ce9d247f65f2777eb965cebdcfbe
Kleps Krzysztof <kkleps@intercity.pl>
2025-03-14 13:42:28 +0100

    Zadanie mosty

--- orly_3.py
+++ orly_3.py
@@ -21,3 +21,20 @@ mean_number = sum(numbers)/len(numbers)
 
 print(round(mean_number))
 
+
+print('Zadanie 2')
+
+def build_bridge(chunk,goal):
+  n = 1
+  while True:
+    most = n * chunk + (n - 1) * chunk / 2
+    if most == goal:
+      return True
+    elif most > goal:
+      return False
+    else:
+      n = n + 1
+
+print(build_bridge(3,8))
+print(build_bridge(10,30))
+print(build_bridge(2, 98))
