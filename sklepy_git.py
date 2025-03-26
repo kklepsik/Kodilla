4ceb955d4965e0d41797dd35735e19cda99ab5ee
Kleps Krzysztof <kkleps@intercity.pl>
2025-03-26 13:47:58 +0100

    Printing shopping

---
+++ sklepy_git.py
@@ -0,0 +1,7 @@
+sklepy = {'warzywniak': ['kartofle', 'marchew', 'koper'],
+          'piekarnia': ['chleb', 'bulki'],
+          'kwiaciarnia': ['bukiet roz']
+          }
+
+for sklep, produkty in sklepy.items():
+    print(f'Idę do {sklep} i kupuje: {produkty}')
