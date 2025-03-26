dadb3be0dacaa5315df30969b20a3106ab28f6f3
Kleps Krzysztof <kkleps@intercity.pl>
2025-03-26 14:00:23 +0100

    Sum products

--- sklepy_git.py
+++ sklepy_git.py
@@ -4,4 +4,8 @@ sklepy = {'warzywniak': ['kartofle', 'marchew', 'koper'],
           }
 
 for sklep, produkty in sklepy.items():
-    print(f'Idę do {sklep} i kupuje: {', '.join(produkty)}')
+    produkty[0] = produkty[0].capitalize()
+    print(f'Idę do {sklep.capitalize()} i kupuje: {', '.join(produkty)}')                    #nie ma w gitcie jakiejś funkcji dodaj ostatnie? by za każdym razem nie wpisywać git add 'nazwa pliku'
+
+suma = sum(len(produkty) for produkty in sklepy.values())
+print(f"Suma produktów: {suma}")
